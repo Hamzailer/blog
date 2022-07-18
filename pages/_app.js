8c18b9269d133684568c7e44ee799d0bc4599314
Hamzailer <Hamzailer@protonmail.com>
2022-07-18 18:55:48 +0500

    Fix google analytics again again

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,31 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import * as gtag from '../utils/gtag.js'
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-import { GA_TRACKING_ID } from '../utils/gtag.js'
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-        </Head>
- 
+        <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
